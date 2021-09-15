c7958e3d7cb3301f102124974d313b667b455624
BSarahB <bslajta@gmail.com>
2021-09-16 00:33:24 +0200

    adding fcts

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define FD_SIZE 10
+# define FD_SIZE 1024
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -9,14 +9,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int	ft_extract_line(char *fd_content, char **line);
+char	*ft_free_line(char **line);
 char	*get_next_line(int fd);
 void	ft_bzero(void *s, size_t len);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, void const *src, size_t len);
+char	*ft_strcat(char *dest, char const *src);
 char	*ft_strjoin(char *s1, char const *s2);
 char	*ft_strchr(char const *s, int c);
-size_t	ft_strlen(char const *s);
 char	*ft_init_string(size_t len);
 char	*ft_strcpy(char *dst, char const *src);
 
